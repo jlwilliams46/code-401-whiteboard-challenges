e5c3c29948f6e83500c3a96f79c94de0d6d9a313
Jamie Williams <jwilliams492@gmail.com>
2018-01-17 21:12:33 -0800

    completed test cases

--- whiteboard-challenge-03/__test__/solution.test.js
+++ whiteboard-challenge-03/__test__/solution.test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const solution = require('../lib/solution');
+require('jest');
+
+describe('solution', function(){
+    it('should begin at the engine and traverse the train, until it reaches the caboose', () => {
+    expect(traverse(engine)).toEqual(12);
+    })
+});
+describe('solution', function(){
+    it('should validate that value is indeed a number', () => {
+    expect(traverse(engine)).$.isNumeric(engine);
+    })
+});
+describe('solution', function(){
+    it('should determine when it has reached a value of null', () => {
+    expect(traverse(engine)).toBe(null);
+    })
+});
