4c216d9bd4a16646d0ff272804a8d1a854dfb8fc
Jamie Williams <jwilliams492@gmail.com>
2018-01-18 23:53:52 -0800

    solution complete

--- whiteboard-challenge-04/lib/solution.js
+++ whiteboard-challenge-04/lib/solution.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const solution = module.exports = {};
+
+let int = [], j, k;
+
+solution.intersect = function (a, b)  {
+    do {
+        do {
+            a[j] === b[k] ? int.push(a[j]) : undefined;
+            k++;
+        } while (b.length);
+        j++;
+    } while (j < a.length);
+    
+    return int;
+    };
