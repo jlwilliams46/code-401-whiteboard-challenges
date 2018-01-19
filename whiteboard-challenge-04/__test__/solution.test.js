49e42fcf51f3911727033fb340f68eb1dae8e80a
Jamie Williams <jwilliams492@gmail.com>
2018-01-19 01:12:05 -0800

    tests completed

--- whiteboard-challenge-04/__test__/solution.test.js
+++ whiteboard-challenge-04/__test__/solution.test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const solution = require('../lib/solution.js');
+
+require ('jest');
+
+describe('array validation', function () {
+    it('should validate the arrays', () => {
+        return true ? (Array.isArray(["mike", "sue", "tom", "kathy", "henry"]) && Array.isArray(["howey", "jim", "sue", "jennifer", "kathy", "hank", "alex"])) === true : null;
+    })
+});
+
+describe('unable to find intersection', function () {
+    it('should return undefined if unable to find intersection', () => {
+        expect(int).toEqual(undefined);
+    })
+});
+
+describe('the correct result', function () {
+    it('should return a result of ["sue", "kathy"]', () => {
+        expect(int(a, b)).toEqual(["sue", "kathy"]);
+    })
+});
