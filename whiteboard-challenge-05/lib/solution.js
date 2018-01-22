5a34580eaf13acad062cfe586d70d37a9ec04c5d
Jamie Williams <jwilliams492@gmail.com>
2018-01-22 01:55:58 -0800

    solution completed

--- whiteboard-challenge-05/lib/solution.js
+++ whiteboard-challenge-05/lib/solution.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const solution = module.exports = {};
+require('jest');
+let a, b;
+
+
+solution.midNode = (head) => {
+    for (a = b = head; b.next && b.next.next; a = a.next, b = b.next.next);
+
+    return a;
+}
+console.log(`output: ${a}`);
