2a727464f7f83383b6309436d991644a1cf35b81
Jamie Williams <jwilliams492@gmail.com>
2018-01-22 23:40:05 -0800

    completed solution and tests.....linter updates and packages

--- whiteboard-challenge-06/lib/solution.js
+++ whiteboard-challenge-06/lib/solution.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const solution = module.exports = {};
+require('jest');
+
+solution.recur = (ct, cb) => {
+  if (ct > 0) return null;
+  if (!cb) return null;
+  if (!ct) return null;
+  if (typeof cb !== 'function') return null;
+  if (typeof ct !== 'number') return null;
+  do {
+    cb();
+    ct--;
+  } while (ct > 0);
+};
