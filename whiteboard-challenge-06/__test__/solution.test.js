f0832f722d535c70acd28314005598652f9f6dd3
jlwilliams46 <jwilliams492@gmail.com>
2018-01-22 23:43:16 -0800

    completed tests

--- whiteboard-challenge-06/__test__/solution.test.js
+++ whiteboard-challenge-06/__test__/solution.test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const solution = require('../lib/solution.js');
+require('jest');
+
+describe('No Input', function () {
+  it('should return null if empty', () => {
+    expect(solution.recur()).toBeNull();
+  });
+});
+describe('No Negative Numbers', function () {
+  it('should return null if number is negative', () => {
+    expect(solution.recur(-100000000)).toBeNull();
+  });
+});
+describe('Invalid Input', function () {
+  it('should return null if input is not a number', () => {
+    expect(solution.recur('awesome sauce', solution.recur)).toBeNull();
+  });
+});
