2fecf14279bd7df7f6832238edd89cb1fda18a31
Jamie Williams <jwilliams492@gmail.com>
2018-01-27 19:15:39 -0800

    incorrect tests rewritten. all tests are now passing.

--- whiteboard-challenge-05/__test__/solution.test.js
+++ whiteboard-challenge-05/__test__/solution.test.js
@@ -3,20 +3,45 @@
 const solution = require('../lib/solution.js');
 require('jest');
 
+let testy = {
+    head: {
+        value: 'jamie',
+        next: {
+            value: 'is',
+            next: {
+                value: 'seriously',
+                next: {
+                    value: 'awesome!',
+                    next: null
+                }
+            }
+        }
+    }
+};
+
 describe('Validate Function', () => {
     it('should confirm that midNode is a function', () => {
         expect(solution.midNode).toBeInstanceOf(Function);
     });
 });
 
-describe('Validate Next Property', () => {
-    it('should check to see if next property is null', () => {
-        expect(solution.midNode()).toBe(null);
+describe('Validate Correct Output', () => {
+    it('should return the middle node in a singly linked list', () => {
+        expect(solution.midNode(testy)).toEqual({
+            "next": {
+                "next": {
+                    "next": null,
+                    "value": "awesome!"
+                },
+                "value": "seriously"
+            },
+            "value": "is"
+        });
     });
 });
 
-describe('Validate Correct Output', () => {
-    it('should return the middle node in a singly linked list', () => {
-        expect(solution.midNode(Head)).toEqual(a);
+describe('Validate Next Property', () => {
+    it('should check to see if next property is null', () => {
+        expect(solution.midNode()).toBe(null);
     });
 });
