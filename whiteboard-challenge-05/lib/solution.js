c7700c200f047a32e2bd941f4de817d39365fb7f
Jamie Williams <jwilliams492@gmail.com>
2018-01-27 19:13:42 -0800

    code completed and variables renamed

--- whiteboard-challenge-05/lib/solution.js
+++ whiteboard-challenge-05/lib/solution.js
@@ -2,12 +2,27 @@
 
 const solution = module.exports = {};
 require('jest');
-let a, b;
 
+solution.midNode = (li) => {
 
-solution.midNode = (head) => {
-    for (a = b = head; b.next && b.next.next; a = a.next, b = b.next.next);
+    if (li === undefined) return null;
+    if (li.head === undefined) return null;
 
-    return a;
-}
-console.log(`output: ${a}`);
+    let ct = 1,
+        nd = li.head,
+        x = 0,
+        it;
+
+    for (it = li.head; it.next; it = it.next) {
+        ct = Math.ceil(ct / 2);
+        ct++;
+        console.log(ct);
+    }
+
+    do {
+        console.log(nd, ct);
+        if (x === ct + -1) return nd;
+        nd = nd.next;
+        x++;
+    } while (x < ct);
+};
