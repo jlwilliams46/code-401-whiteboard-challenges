51f45124c43458d57348fc524460ae5342b58c17
Jamie Williams <jwilliams492@gmail.com>
2018-01-29 12:27:19 -0800

    solution complete

--- whiteboard-challenge-07/lib/solution.js
+++ whiteboard-challenge-07/lib/solution.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const solution = module.exports = {};
+
+solution.checkLink = (checkLink) => {
+  let slow = checkLink, fast = checkLink;
+
+  if (checkLink === undefined) throw new Error('Undefined!');
+  if (typeof checkLink !== 'object') throw new Error('Not An Object!');
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (fast === slow) return true;
+  }
+  return false;
+};
