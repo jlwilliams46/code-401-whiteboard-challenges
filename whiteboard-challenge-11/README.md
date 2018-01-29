16b78617bbe2b6d87ed774b35b8be4cc073fe08a
Jamie Williams <jwilliams492@gmail.com>
2018-01-29 15:04:16 -0800

    completed pseudocode, solution, andtests. all tests are passing

---
+++ whiteboard-challenge-11/README.md
@@ -0,0 +1,6 @@
+# Problem Domain
+##### January 29, 2018
+&nbsp;
+Complete today's whiteboard challenge and follow the submission instructions below:
+
+You have an integer array which contains numbers from 1 to 100 but one number is missing, you need to write a function calculateMissing = (array) => {...} to find that missing number in an array.
---
+++ whiteboard-challenge-11/__test__/solution.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const solution = require('../lib/solution.js');
+
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+
+describe('calculateMissing Tests', () => {
+  describe('Correct Answer', () => {
+    test('should return the correct missing number', () => {
+      expect(() => solution.calculateMissing(testArray).toEqual(12));
+    });
+  });
+
+  describe('Array Validation', () => {
+    test('should verify that the argument is an array', () => {
+      expect(() => solution.calculateMissing(Array.isArray(testArray).toEqual(true)));
+    });
+  });
+
+  describe('Empty Array', () => {
+    test('should return null if array is empty', () => {
+      expect(() => solution.calculateMissing(testArray).toBeNull());
+    });
+  });
+});
