3fc848943b5a9a88a4753fcea97a1a619b885e32
Jamie Williams <jwilliams492@gmail.com>
2018-02-19 21:01:39 -0800

    solution complete, fixed linter formatting errors

--- whiteboard-challenge-26/lib/utils.js
+++ whiteboard-challenge-26/lib/utils.js
@@ -3,12 +3,12 @@
 const utils = module.exports = {};
 
 utils.map = (arr, cb) => {
-  if(!arr || arr instanceof Array === false) return null;
-  if(!cb || cb instanceof Object === false) return null;
+  if (!arr || arr instanceof Array === false) return null;
+  if (!cb || cb instanceof Object === false) return null;
+  let clone = new Array(...arr),
+    output = [];
 
-  let clone = new Array(...arr), output = [];
-
-  for(let x in clone) {
+  for (let x in clone) {
     output.push(cb(arr[x]));
   }
 
@@ -16,25 +16,25 @@ utils.map = (arr, cb) => {
 };
 
 utils.filter = (arr, cb) => {
-  if(!arr || arr instanceof Array === false) return null;
-  if(!cb || cb instanceof Object === false) return null;
+  if (!arr || arr instanceof Array === false) return null;
+  if (!cb || cb instanceof Object === false) return null;
+  let clone = new Array(...arr),
+    output = [];
 
-  let clone = new Array(...arr), output = [];
-
-  for(let x in clone) {
-    if(arr[x] === cb()) output.push(arr[x]);
+  for (let x in clone) {
+    if (arr[x] === cb()) output.push(arr[x]);
   }
 
   return output;
 };
 
 utils.reduce = (arr, cb, n) => {
-  if(!arr || arr instanceof Array === false) return null;
-  if(!cb || cb instanceof Object === false) return null;
+  if (!arr || arr instanceof Array === false) return null;
+  if (!cb || cb instanceof Object === false) return null;
+  let clone = new Array(...arr),
+    output = n;
 
-  let  clone = new Array(...arr), output = n;
-
-  for(let x in clone) {
+  for (let x in clone) {
     output = cb(output, clone[x]);
   }
 
