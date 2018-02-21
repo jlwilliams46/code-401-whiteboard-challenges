44c5f29a415a6b60c25ccd72197bc51c7a9392a0
Jamie Williams <jwilliams492@gmail.com>
2018-02-21 09:21:44 -0800

    completed solution and readme

---
+++ timed-challege/lib/solution.js
@@ -0,0 +1,21 @@
+'use strict';
+// Big O Notation: O(n^2)
+function highest(x) {
+    let a = x.split(' '),
+        b = 0,
+        c = '';
+    for (let i = 0; i < a.length; i++) {
+        let s = a[i],
+            d = 0;
+
+        for (let j = 0; j < s.length; j++) {
+            d += (s.charCodeAt(j) - 96);
+        }
+
+        if (d > b) {
+            b = d;
+            c = s;
+        }
+    }
+    return c;
+}
