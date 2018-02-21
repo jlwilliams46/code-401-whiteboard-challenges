0c14c232f982ab2f520a084c15e9bea69edb69b0
Jamie Williams <jwilliams492@gmail.com>
2018-02-20 23:34:44 -0800

    completed solution and readme with all tests passing

--- whiteboard-challenge-27/README.md
+++ whiteboard-challenge-27/README.md
@@ -1,6 +1,8 @@
 # Whiteboard Challenge 27
 ##### Date: February 20, 2018
 &nbsp;
-## Problem Domain: 
+## Problem Domain:
+** Big 0 Notation **: O(n^2)
+ 
 &nbsp;
 Given a 2-dimensional array of numbers, rotate the array 90 degrees.
--- whiteboard-challenge-27/__test__/solution.test.js
+++ whiteboard-challenge-27/__test__/solution.test.js
@@ -1,1 +1,71 @@
 'use strict';
+
+const solution = require('../lib/solution');
+
+let testArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  testFloat = [
+    [0.97344, 0.09209, 0.92846],
+    [0.25985, 0.78799, 0.68529],
+    [0.75178, 0.26189, 0.04946],
+  ], 
+  testUndefined = [];
+
+describe('Correct Result', () => {
+  it('should return the correct result', () => {
+    expect(solution.rotate(testArray)).toEqual([
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ]);
+  });
+});
+
+describe('Floating Point Integers', () => {
+  it('should return correct result for floating point integers', () => {
+    expect(solution.rotate(testFloat)).toEqual([
+      [0.97344, 0.25985, 0.75178],
+      [0.09209, 0.78799, 0.26189],
+      [0.92846, 0.68529, 0.04946],
+    ]);
+  });
+});
+
+describe('Function Operation', () => {
+  it('should verify that solution.rotate correctly returns result', () => {
+    expect(solution.rotate()).toBeDefined();
+  });
+});
+
+describe('No Arguments', () => {
+  it('should return null if there are no arguments present', () => {
+    expect(solution.rotate()).toBeNull();
+  });
+});
+
+describe('Empty Array', () => {
+  it('should throw error of array is empty', () => {
+    expect(solution.rotate([])).toThrow;
+  });
+});
+
+describe('Array Undefined', () => {
+  it('should throw error if array is undefined', () => {
+    expect(solution.rotate(testUndefined)).toThrow;
+  });
+});
+
+describe('Array Validation', () => {
+  it('should return null for anything other than an array', () => {
+    expect(solution.rotate('JAMIE RULES!')).toBeNull();
+  });
+});
+
+describe('Function Validation', () => {
+  it('should verify that solution.rotate is a function', () => {
+    expect(solution.rotate).toBeInstanceOf(Function);
+  });
+});
